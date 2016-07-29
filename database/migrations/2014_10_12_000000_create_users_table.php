4ba7e30951e48120e3cc62359b1e94f19b097709
Vladimir Khonin <Vladimir.Khonin@Megafon.ru>
2016-07-29 19:03:18 +0500

    правки юзера и команды финиш

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,37 +12,41 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_status', function (Blueprint $table) {
+        Schema::create('UserStatus', function (Blueprint $table) {
             $table->increments('id')->comment('Статус пользователя');
             $table->string('name');
+            $table->softDeletes()->comment('дата удаления');
         });
 		
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('User', function (Blueprint $table) {
             $table->increments('id')->comment('Пользователи');
             $table->string('login')->nullable()->comment('Логин')->unique();
             $table->string('name')->nullable()->comment('ФИО или имя команды')->unique();
             $table->string('email')->unique();
             $table->string('password');
-            $table->integer('user_type')->default(1)->comment('1 - пользователь, 2 - команда, 3 - робот');
-            $table->integer('user_status')->default(1)->unsigned()->comment('Статус пользователя. 1-активен, 0-не активен');
-            $table->foreign('user_status')->references('id')->on('user_status');
+            $table->integer('userType')->default(1)->comment('1 - пользователь, 2 - команда, 3 - робот');
+            $table->integer('userStatusId')->default(1)->unsigned()->comment('Статус пользователя. 1-активен, 0-не активен');
+            $table->foreign('userStatusId')->references('id')->on('UserStatus');
             $table->string('psLogin')->unique()->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes()->comment('дата удаления');
         });
 		
-        Schema::create('user_profile', function (Blueprint $table) {
+        Schema::create('UserProfile', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('login');
-            $table->string('name');
+            $table->integer('userId')->unsigned();
+            $table->foreign('userId')->references('Id')->on('User'->comment('ключ на таблицу User');
+            $table->binary('photo');
             $table->timestamps();
         });
         Schema::create('TeamUser', function (Blueprint $table) {
             $table->increments('Id');
+            $table->integer('userId')->unsigned();
             $table->foreign('userId')->references('Id')->on('User'->comment('ключ на таблицу User');
+            $table->integer('teamUserId')->unsigned();//команда
+            $table->foreign('teamUserId')->references('Id')->on('User'->comment('ключ на таблицу User');
             $table->boolean('isLeader')->nullable()->comment('маркер капитана');
-            $table->boolean('isActive')->default(true)->comment('маркер жив\мертв');
-            $table->date('startDate')->default(true)->comment('маркер жив\мертв');
             $table->timestamps()->comment('старт.стоп дата');
             $table->softDeletes()->comment('дата удаления');
         });
@@ -55,8 +59,9 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('user_profile');
-        Schema::drop('users');
-        Schema::drop('user_status');
+        Schema::drop('TeamUser');
+        Schema::drop('UserProfile');
+        Schema::drop('User');
+        Schema::drop('UserStatus');
     }
 }
