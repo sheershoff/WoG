df3415adee80e450b460d465519a2c66e5833b91
Vladimir Khonin <Vladimir.Khonin@Megafon.ru>
2016-07-29 17:13:50 +0500

    таблички user, userProfile, UserStatus Ok

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,14 +12,29 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::create('user_status', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+        });
+		
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('login');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('user_type')->default(1);
+            $table->integer('user_status')->default(1)->unsigned();
+            $table->foreign('user_status')->references('id')->on('user_status');
             $table->string('psLogin')->unique()->nullable();
             $table->rememberToken();
+            $table->timestamps();
+        });
+		
+        Schema::create('user_profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('login');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -31,6 +46,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        Schema::drop('user_profile');
         Schema::drop('users');
+        Schema::drop('user_status');
     }
 }
--- database/migrations/2016_07_22_035900_create_skills_table.php
+++ database/migrations/2016_07_22_035900_create_skills_table.php
@@ -17,6 +17,8 @@ class CreateSkillsTable extends Migration
             $table->string('name')->unique()->comment('Наименование');
             $table->text('comment')->nullable();
             $table->json('options')->nullable()->comment('прочие настройки');
+            $table->integer('currency_id')->unsigned()->comment('Связка с валютой в которой считается навык у пользователя');
+            $table->foreign('currency_id')->references('id')->on('currencies');
             $table->timestamps();
         });
     }
