2e18bfdae5259cf4d98db9f5ba6570c485d655dc
Vladimir Khonin <Vladimir.Khonin@Megafon.ru>
2016-07-29 17:33:20 +0500

    role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,18 +13,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('user_status', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->comment('Статус пользователя');
             $table->string('name');
         });
 		
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('login');
-            $table->string('name');
+            $table->increments('id')->comment('Пользователи');
+            $table->string('login')->nullable()->comment('Логин')->unique();
+            $table->string('name')->nullable()->comment('ФИО или имя команды')->unique();
             $table->string('email')->unique();
             $table->string('password');
-            $table->integer('user_type')->default(1);
-            $table->integer('user_status')->default(1)->unsigned();
+            $table->integer('user_type')->default(1)->comment('1 - пользователь, 2 - команда, 3 - робот');
+            $table->integer('user_status')->default(1)->unsigned()->comment('Статус пользователя. 1-активен, 0-не активен');
             $table->foreign('user_status')->references('id')->on('user_status');
             $table->string('psLogin')->unique()->nullable();
             $table->rememberToken();
