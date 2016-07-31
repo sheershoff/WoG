2c79db6004f357d4e8e315635f02901287f6b7ea
Ansible IT Automation <ansible@url-wog-app01.megafon.ru>
2016-07-31 20:37:19 +0300

    прочие правки

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,29 +11,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//	$this->call(UsersTableSeeder::class);
-//        $this->call(UsersTableSeeder::class);
-//        $this->call(RolesTableSeeder::class);
-//        $this->call(CurrenciesTableSeeder::class);
-        $this->call('CurrencyTrancactionTableSeeder');
         $this->call('UserStatusTableSeeder');
-        $this->call('TeamUserTableSeeder');
-        $this->call('UserProfileTableSeeder');
-        $this->call('RoleUserTableSeeder');
+        $this->call('UsersTableSeeder');
+        $this->call('PagesTableSeeder');
         $this->call('CurrencyTypesTableSeeder');
-        $this->call('QuestTableSeeder');
         $this->call('RoleTableSeeder');
-        $this->call('CurrencyTableSeeder');
         $this->call('UserQuestStatusTableSeeder');
+        $this->call('JobsTableSeeder');
+        $this->call('UserProfileTableSeeder');
+        $this->call('TeamUserTableSeeder');
+        $this->call('QuestTableSeeder');
+        $this->call('RoleUserTableSeeder');
+        $this->call('CurrencyTableSeeder');
+        $this->call('SkillTableSeeder');
         $this->call('BalanceTableSeeder');
+        $this->call('ActionTableSeeder');
         $this->call('UserQuestTableSeeder');
         $this->call('ActionCurrencyTableSeeder');
         $this->call('MailTemplateTableSeeder');
-        $this->call('ActionTableSeeder');
         $this->call('ActionTrancactionTableSeeder');
-        $this->call('JobsTableSeeder');
-        $this->call('SkillTableSeeder');
-        $this->call('PagesTableSeeder');
+        $this->call('CurrencyTrancactionTableSeeder');
         $this->call('UserSkillTableSeeder');
     }
 }
