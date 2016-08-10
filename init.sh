f522745513e8f0a9bc803bab08fec092e6743ae6
xVlady <xw@ya.ru>
2016-08-10 10:31:46 +0300

    gulp и bower работают

---
+++ init.sh
@@ -0,0 +1,15 @@
+#устанавливаем окружение
+php artisan down
+
+npm update
+bower update
+composer update
+
+npm link gulp
+npm link laravel-elixir
+
+gulp
+#gulp --production
+
+artisan serve --host= --port=9999
+php artisan up
