a735483bd8cf74f92e9392af46081ef819d435dd
CrazyHappy <crazyhappy@bk.ru>
2016-08-19 18:03:32 +1000

    Правки в админки
    Все по дизайну

--- resources/views/admin/form/menu.blade.php
+++ resources/views/admin/form/menu.blade.php
@@ -1,9 +1,9 @@
 <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview</a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Export</a></li>
+            {{-- <li class="active"><a href="#">Overview</a></li> --}}
+            <li><a href="#">Квесты </a></li>
+            <li><a href="#">Скилы</a></li>
+            <li><a href="#">Магазин </a></li>
           </ul>
           <ul class="nav nav-sidebar">
             <li><a href="">Nav item</a></li>
