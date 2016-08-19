1117779416e7e72f5c1918f2b7ab2c47eed46652
Vlady <xw@ya.ru>
2016-08-19 09:44:25 +0500

    init.sh
    несколько комментариев и пояснений

--- init.sh
+++ init.sh
@@ -7,16 +7,18 @@ npm update
 bower update
 composer update
 
-php artisan migrate
-php artisan migrate --seed
-php artisan vendor:publish
-#как минимум двигаем последовательности
-./vendor/bin/phpunit
+php artisan migrate:reset #сносим вседанные
+php artisan migrate #заливаем обратно структуру данных
+php artisan migrate --seed #заливаем данных
+
+php artisan vendor:publish #не опубликованые данные компонентов, обычно не требуется
+
+./vendor/bin/phpunit #как минимум двигаем последовательности
 
 npm link gulp
 npm link laravel-elixir
 
-gulp
+gulp #вытаскиваем из vendor/bower_dl/ то что нужно в паблик, обычно не требуется
 #gulp --production
 
 artisan serve --host= --port=9999
