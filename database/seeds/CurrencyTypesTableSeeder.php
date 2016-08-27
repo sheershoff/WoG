e628b219da7cf7fad2f2bbba6c3bade0decf4953
xVlady <xw@ya.ru>
2016-08-27 10:07:09 +0300

    iseed пришлось чинить под л53 + выгрузки заново

--- database/seeds/CurrencyTypesTableSeeder.php
+++ database/seeds/CurrencyTypesTableSeeder.php
@@ -14,8 +14,64 @@ class CurrencyTypesTableSeeder extends Seeder
     {
         
 
-        \DB::table('currency_types')->delete();
+        \DB::connection('pgsql')->table('currency_types')->delete();
         
+        \DB::connection('pgsql')->table('currency_types')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'Карма',
+                'unit' => 'СуперМегаСпасибо',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Голда',
+                'unit' => 'Тугриков',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            2 => 
+            array (
+                'id' => 10,
+                'name' => 'Рейтинг',
+                'unit' => 'XP',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            3 => 
+            array (
+                'id' => 9,
+                'name' => 'Медаль',
+                'unit' => 'Шт',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            4 => 
+            array (
+                'id' => 4,
+                'name' => 'Шмот',
+                'unit' => 'шт',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            5 => 
+            array (
+                'id' => 3,
+                'name' => 'Сноровка',
+                'unit' => 'Звезд',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+        ));
         
         
     }
--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -14,8 +14,98 @@ class RolesTableSeeder extends Seeder
     {
         
 
-        \DB::table('roles')->delete();
+        \DB::connection('pgsql')->table('roles')->delete();
         
+        \DB::connection('pgsql')->table('roles')->insert(array (
+            0 => 
+            array (
+                'id' => -1,
+                'name' => 'Admin',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            1 => 
+            array (
+                'id' => -2,
+                'name' => 'Guest',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            2 => 
+            array (
+                'id' => 8,
+                'name' => 'lvl1',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            3 => 
+            array (
+                'id' => 5,
+                'name' => 'lvl3',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            4 => 
+            array (
+                'id' => 6,
+                'name' => 'lvl4',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            5 => 
+            array (
+                'id' => 7,
+                'name' => 'lvl5',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            6 => 
+            array (
+                'id' => 1,
+                'name' => 'Дух',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            7 => 
+            array (
+                'id' => 2,
+                'name' => 'Солдат',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            8 => 
+            array (
+                'id' => 4,
+                'name' => 'Командир',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            9 => 
+            array (
+                'id' => 3,
+                'name' => 'Эксперт',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            10 => 
+            array (
+                'id' => 9,
+                'name' => 'Skill',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+        ));
         
         
     }
