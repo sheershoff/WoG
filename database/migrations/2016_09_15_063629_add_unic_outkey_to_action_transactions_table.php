8cc86a7ebe95f17f3959bab6c9f1e5b4972c8122
xVlady <xw@ya.ru>
2016-09-15 09:51:15 +0300

    миграции - рабочие часы, макс валюты, уникальный внешний ключ для лога действий

---
+++ database/migrations/2016_09_15_063629_add_unic_outkey_to_action_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUnicOutkeyToActionTransactionsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('action_transactions', function (Blueprint $table) {
+            $table->integer('uni_quest_id')->unsigned()->nullable->comment('квест, которому принадлежит Action. Заполняется только если заполняется uni_outkey');
+            $table->foreign('uni_quest_id')->references('id')->on('quests')->onDelete('cascade');
+            $table->integer('uni_outkey')->unsigned()->nullable->comment('Уникальный для квеста внешний ключ. Используется если нужно добиться уникальности внешних заданий (проверить - назначени за это или нет). Заполняется совместно с uni_quest_id');
+            $table->unique(['uni_quest_id', 'uni_outkey']); //уникальный ключ? если нужно соблюсти уникальность и не вставлять действия за уже совершённые действия
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('action_transactions', function (Blueprint $table) {
+            $table->dropUnique('action_transactions_uni_quest_id_uni_outkey_unique');
+            $table->dropColumn('end_worktime');
+            $table->dropColumn('end_worktime');
+        });
+    }
+
+}
